db746d612000939a6d0aaf197ee29813a3a86e24
VP Tervonen <elamshin@protonmail.com>
2022-09-13 13:06:36 +0300

    2.1 luotu

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,83 @@
+const Course = (props) => {
+  const { course } = props
+  console.log(course)
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
+}
+
+const Content = ({ parts }) => {
+  const p = parts
+  return (
+    <div>
+        {p.map(part =>
+          <Part key={part.id} name={part.name} exercise={part.exercises} />
+          )}
+    </div>
+  )
+}
+
+const Part = ({ name, exercise}) => {
+  return (
+    <p>{name} {exercise}</p>
+  )
+}
+
+/*
+
+const Total = (props) => {
+  
+  const t = props.parts.reduce((previousValue,currentValue) => previousValue + currentValue.exercises,0,);
+
+  return (
+    <div>
+      <p>
+        Number of exercises {t}
+      </p>
+    </div>
+  )
+}*/
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
