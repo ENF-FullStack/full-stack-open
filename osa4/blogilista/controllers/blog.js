65159d386b243d107ebbebb5757b69abcae97911
VP Tervonen <elamshin@protonmail.com>
2022-09-26 10:19:49 +0300

    4.2 luotu

---
+++ osa4/blogilista/controllers/blog.js
@@ -0,0 +1,28 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+blogRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    name: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
+
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.json(savedBlog)
+    })
+    .catch((error) => next(error))
+})
+
+module.exports = blogRouter
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,50 +1,11 @@
-// const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
 
-app.use(express.json())
-app.use(cors())
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+const server = http.createServer(app)
 
-const password = process.argv[2]
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = `mongodb+srv://fullstack:${password}@cluster0.scofuju.mongodb.net/bloglist?retryWrites=true&w=majority`
-
-// const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl).catch((error) => {
-  console.log('Error: ', error.message)
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
