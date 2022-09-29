3a7a931b967284a7747206e955595b77ac11319f
VP Tervonen <elamshin@protonmail.com>
2022-09-29 06:50:01 +0300

    4.8 luotu

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlog = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlog[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlog[1])
+  await blogObject.save()
+})
+
+test('all blog entries are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlog.length)
+})
+
+test('specific blog entry is in array', async () => {
+  const response = await api.get('/api/blogs')
+
+  const title = response.body.map((r) => r.title)
+
+  expect(title).toContain('Go To Statement Considered Harmful')
+})
+
+test('correct amount of blog entries', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first blog is named React patterns', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].title).toBe('React patterns')
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
