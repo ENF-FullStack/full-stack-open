59551eeb448e32800b152e48e17cb53f163460e7
VP Tervonen <elamshin@protonmail.com>
2022-09-30 07:49:06 +0300

    4.10 - 4.12 luotu

--- osa4/blogilista/controllers/blog.js
+++ osa4/blogilista/controllers/blog.js
@@ -2,26 +2,35 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogRouter.get('/', async (request, response) => {
-  const blog = await Blog.find({})
-  response.json(blog)
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response, next) => {
+blogRouter.post('/', async (request, response) => {
   const body = request.body
 
   const blog = new Blog({
-    name: body.title,
+    title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
   })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.json(savedBlog)
-    })
-    .catch((error) => next(error))
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogRouter
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,40 +1,23 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialBlog = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-]
-
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlog[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlog[1])
-  await blogObject.save()
+  await Blog.insertMany(helper.initialBlog)
+
+  // const blogObjects = helper.initialBlog.map((blog) => new Blog(blog))
+  // const promiseArray = blogObjects.map((blog) => blog.save())
+  // await Promise.all(promiseArray)
 })
 
 test('all blog entries are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialBlog.length)
+  expect(response.body).toHaveLength(helper.initialBlog.length)
 })
 
 test('specific blog entry is in array', async () => {
@@ -59,8 +42,87 @@ test('the first blog is named React patterns', async () => {
 
 test('unique identifier id exists', async () => {
   const response = await api.get('/api/blogs')
-
+  console.log('arvo:', response.body[0].id)
   expect(response.body[0].id).toBeDefined()
+})
+
+test('adding new blog item to db', async () => {
+  const newBlog = {
+    title: 'post to mongo db',
+    author: 'VP',
+    url: 'http://mongo.db',
+    likes: 11,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlog.length + 1)
+
+  const contents = blogsAtEnd.map((r) => r.title)
+  expect(contents).toContain('post to mongo db')
+})
+
+test('a specific blog item can be viewed', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToView = blogsAtStart[0]
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const processedBlogToView = JSON.parse(JSON.stringify(blogToView))
+  expect(resultBlog.body).toEqual(processedBlogToView)
+})
+
+test('a note can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlog.length - 1)
+
+  const contents = blogsAtEnd.map((r) => r.title)
+  expect(contents).not.toContain(blogToDelete.title)
+})
+
+test('blog post with no likes > set to 0', async () => {
+  const newBlog = {
+    title: 'post to mongo db',
+    author: 'VP',
+    url: 'http://mongo.db',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlog.length + 1)
+
+  const contents = blogsAtEnd.map((r) => r.likes)
+  expect(contents).toContain(0)
+})
+
+test('blog without title and url refused', async () => {
+  const newBlog = {
+    author: 'VP',
+    likes: 12,
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlog.length)
 })
 
 afterAll(() => {
