94bf9b190078c5d0dfa5168ecc515114892c6060
VP Tervonen <elamshin@protonmail.com>
2022-10-05 18:16:18 +0300

    5.7 luotu

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,49 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author} <br />
-    Url: {blog.url} <br />
-    Likes: {blog.likes}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog, user }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  if (showDetails === false) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} by {blog.author}
+          <button id="show" onClick={toggleDetails}>
+            View
+          </button>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} by {blog.author}
+          <button id="hide" onClick={toggleDetails}>
+            Hide
+          </button>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          Likes: {blog.likes}
+          <button id="likebutton">Like</button>
+        </div>
+        <div>{user.name}</div>
+      </div>
+    )
+  }
+}
 
 export default Blog
