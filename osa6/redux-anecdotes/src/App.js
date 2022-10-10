3acaf42856af31a5871d30a19f888e3539691a5b
VP Tervonen <elamshin@protonmail.com>
2022-10-11 01:20:01 +0300

    6.5 päivitetty

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -23,7 +23,7 @@ const addAnecdote = (ev) => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.sort((a,b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
