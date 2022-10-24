0225c58bc574dfec63e4186b218f9a99e8010b52
VP Tervonen <elamshin@protonmail.com>
2022-10-24 09:13:51 +0300

    7.15 päivitystä

--- osa7/bloglist/client/src/components/BlogList.js
+++ osa7/bloglist/client/src/components/BlogList.js
@@ -4,17 +4,17 @@ import { useSelector } from 'react-redux'
 const BlogList = () => {
   const blogs = useSelector((state) => state.blogs)
   const id = useParams().id
-  console.log(blogs)
   const list = blogs.filter((blog) => blog.user.id === id)
-  console.log(list)
   return (
     <div>
-      <h2>Blogs by </h2>
-      {list.map((blog) => (
-        <p key={blog.id}>
-          {blog.title} by {blog.author}
-        </p>
-      ))}
+      <h2>added blogs</h2>
+      <ul>
+        {list.map((blog) => (
+          <li key={blog.id}>
+            {blog.title} by {blog.author}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
