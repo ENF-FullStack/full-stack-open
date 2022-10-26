d3bca8f0f89321db89201593d2d4050c89cd41cb
VP Tervonen <elamshin@protonmail.com>
2022-10-26 18:23:50 +0300

    8.9 luotu

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -3,23 +3,16 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS, ALL_BOOKS } from './components/queries'
 
-const ALL_AUTHORS = gql`
-query {
-    allAuthors {
-        name
-        born
-        bookCount
-    }
-}
-`
 const App = () => {
   const [page, setPage] = useState('authors')
 
-  const result = useQuery(ALL_AUTHORS)
+  const authorQuery = useQuery(ALL_AUTHORS)
+  const bookQuery = useQuery(ALL_BOOKS)
 
-  if (result.loading) {
+  if (authorQuery.loading || bookQuery.loading ) {
     return <div>loading...</div>
   }
 
@@ -31,9 +24,9 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors authors={result.data.allAuthors} show={page === 'authors'} />
+      <Authors authors={authorQuery.data.allAuthors} show={page === 'authors'} />
 
-      <Books show={page === 'books'} />
+      <Books books={bookQuery.data.allBooks} show={page === 'books'} />
 
       <NewBook show={page === 'add'} />
     </div>
