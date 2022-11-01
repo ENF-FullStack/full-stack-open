c08f9d50b1c7b04bd3503a1a90ccd5e8d35fc09e
VP Tervonen <elamshin@protonmail.com>
2022-11-01 12:19:40 +0200

    8.20 toimii

--- osa8/library-frontend/src/components/Recommend.js
+++ osa8/library-frontend/src/components/Recommend.js
@@ -1,26 +1,16 @@
-// import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ME, ALL_BOOKS } from './queries'
 
 const Recommend = (props) => {
-  // const [favBooks, result] = useLazyQuery(FAV_BOOKS)
-  // const [me, setMe] = useState(null)
-  // const [myBooks, setMyBooks] = useState([])
   const { data: meData } = useQuery(ME)
 
   const { data: booksData } = useQuery(ALL_BOOKS, {
     fetchPolicy: 'no-cache',
     variables: { genre: meData?.me?.favoriteGenre },
     skip: !meData?.me?.favoriteGenre,
-    // onCompleted: setMyBooks,
   })
   console.log('booksData: ', booksData)
   console.log(booksData?.allBooks)
-  // const filterBooks = Object.values(booksData?.allBooks)
-  // console.log(filterBooks)
-
-  // const allBooks = booksData.allBooks
-  // console.log('allBooks: ', allBooks)
 
   if (!props.show) {
     return null
@@ -37,20 +27,13 @@ const Recommend = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {/* {filterBooks.map((b) => {
+          {booksData?.allBooks.map((b) => (
             <tr key={b.title}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
               <td>{b.published}</td>
             </tr>
-          })} */}
-          {booksData?.allBooks.map((b) => {
-            <tr key={b.title}>
-              <td>{b.title}</td>
-              <td>{b.author.name}</td>
-              <td>{b.published}</td>
-            </tr>
-          })}
+          ))}
         </tbody>
       </table>
     </div>
