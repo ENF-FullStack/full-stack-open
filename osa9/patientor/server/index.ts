853936a190d22df9994ae30b36268dd9e978d01a
VP Tervonen <elamshin@protonmail.com>
2022-11-03 11:16:27 +0200

    9.9 client lisätty

--- osa9/patientor/server/index.ts
+++ osa9/patientor/server/index.ts
@@ -1,10 +1,14 @@
-import express from 'express';
+import express, { Request } from 'express';
+import cors = require('cors');
+
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors<Request>());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res)=> {
+app.get('/api/ping', (_req, res)=> {
     console.log('got pinged, sending pong');
     res.send('pong');
 });
