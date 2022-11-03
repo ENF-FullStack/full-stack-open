3c756f09362a27febb0f9f351df12fef2df7d247
VP Tervonen <elamshin@protonmail.com>
2022-11-03 05:56:46 +0200

    9.7 luotu

--- osa9/exerciseCalculator.ts
+++ osa9/exerciseCalculator.ts
@@ -1,31 +1,31 @@
 interface Output {
-  periodAmount: number
-  trainingDays: number
-  success: boolean
-  target: number
-  rating: number
-  average: number
-  rateDescr: string
+  periodAmount: number;
+  trainingDays: number;
+  success: boolean;
+  target: number;
+  rating: number;
+  average: number;
+  rateDescr: string;
 }
 
 const calculateExercises = (exerciseLog: number[], target: number): Output => {
-  const periodAmount = exerciseLog.length
+  const periodAmount = exerciseLog.length;
   const trainingDays = exerciseLog.reduce(
     (total, day) => total + (day > 0 ? 1 : 0),
     0
-  )
+  );
 
-  const totalHours = exerciseLog.reduce((total, hours) => total + hours, 0)
-  const average = totalHours / periodAmount
-  const rating = average >= target + 0.5 ? 3 : average >= target ? 2 : 1
-  let rateDescr = ''
+  const totalHours = exerciseLog.reduce((total, hours) => total + hours, 0);
+  const average = totalHours / periodAmount;
+  const rating = average >= target + 0.5 ? 3 : average >= target ? 2 : 1;
+  let rateDescr = '';
 
   if (average === target) {
-    rateDescr = 'Good job!'
+    rateDescr = 'Good job!';
   } else if (average > 0.75 * target) {
-    rateDescr = 'Almost there, keep it up!'
+    rateDescr = 'Almost there, keep it up!';
   } else {
-    rateDescr = 'You are a slob, get moving!'
+    rateDescr = 'You are a slob, get moving!';
   }
 
   return {
@@ -36,25 +36,28 @@ const calculateExercises = (exerciseLog: number[], target: number): Output => {
     rateDescr,
     target,
     average
-  }
-}
+  };
+};
 
+export { calculateExercises };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const parseInputExercises = (targetRaw: any, exercisesRaw: any[]) => {
-  if (!targetRaw || exercisesRaw.length === 0) throw 'Missing parameters!'
+  if (!targetRaw || exercisesRaw.length === 0) throw 'Missing parameters!';
 
-  const exerciseLog = exercisesRaw.map((arr) => parseFloat(arr))
-  const target = parseFloat(targetRaw)
+  const exerciseLog = exercisesRaw.map((arr) => parseFloat(arr));
+  const target = parseFloat(targetRaw);
 
   if (Number.isNaN(target) || exerciseLog.some((arr) => isNaN(arr))) {
-    throw 'NaN parameters!'
+    throw 'NaN parameters!';
   }
 
-  return calculateExercises(exerciseLog, target)
-}
+  return calculateExercises(exerciseLog, target);
+};
 
 if (process.argv.length > 2) {
-  const [, , target, ...exerciseLog] = process.argv
-  console.log(parseInputExercises(target, exerciseLog))
+  const [, , target, ...exerciseLog] = process.argv;
+  // eslint-disable-next-line no-console
+  console.log(parseInputExercises(target, exerciseLog));
 }
 
 // 2 1 0 2 4.5 0 3 1 0 4
--- osa9/index.ts
+++ osa9/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
+import { isArray } from 'util';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
 app.use(express.json());
@@ -31,6 +33,45 @@ app.get('/bmi', (req: Request, res: Response) => {
   };
 
   res.json(bmi);
+});
+
+app.post('/exercise', (req: Request, res: Response) => {
+  const { daily_exercise, target } = req.body as {
+    daily_exercise: number[];
+    target: number;
+  };
+
+  if (!daily_exercise || !target) {
+    res.status(400).send({ error: 'Not enough args, check: post' });
+  }
+
+  if (Number.isNaN(target)) {
+    res.status(400).send({ error: 'Not enough args, check: target' });
+  }
+
+  try {
+    if (!isArray(daily_exercise)) throw new Error();
+
+    daily_exercise.forEach((hour) => {
+      if (Number.isNaN(hour)) throw new Error();
+    });
+  } catch (error) {
+    res.status(400).send({
+      error: 'malformed params, check: array'
+    });
+  }
+
+  const result: {
+    periodAmount: number;
+    trainingDays: number;
+    success: boolean;
+    target: number;
+    rating: number;
+    average: number;
+    rateDescr: string;
+  } = calculateExercises(daily_exercise, target);
+
+  res.json(result);
 });
 
 const PORT = 3002;
