0911d4edfb84b8ccf51d650c4dcabd5e63f6a55d
VP Tervonen <elamshin@protonmail.com>
2022-11-07 07:17:11 +0200

    9.14 luotu

--- osa9/cra_typescript/src/index.tsx
+++ osa9/cra_typescript/src/index.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+import Header from './components/Header';
+import Content from './components/Content';
+import Total from './components/Total';
+
+const App: React.FC = () => {
+  const courseName = 'Half Stack application development';
+  const courseParts = [
+    {
+      name: 'Fundamentals',
+      exerciseCount: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exerciseCount: 7
+    },
+    {
+      name: 'Deeper type usage',
+      exerciseCount: 14
+    }
+  ];
+
+  const totalParts = courseParts.reduce(
+    (total, part) => total + part.exerciseCount,
+    0
+  );
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total total={totalParts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
