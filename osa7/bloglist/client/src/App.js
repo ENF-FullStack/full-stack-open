15846edf458f95dad98b1cf8e73c2b8dab93a480
VP Tervonen <elamshin@protonmail.com>
2022-11-13 21:23:37 +0200

    7.15 siivousta

--- osa7/bloglist/client/src/App.js
+++ osa7/bloglist/client/src/App.js
@@ -1,31 +1,21 @@
 /* eslint-disable no-unused-vars */
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Notification from './components/Notification'
-// import LoginForm from './components/LoginForm'
-// import Togglable from './components/Togglable'
 import BlogList from './components/BlogList'
 import Main from './components/Main'
 import UserList from './components/UserList'
-
-import { Routes, Route, Link } from 'react-router-dom'
-
 import blogService from './services/blogs'
-// import loginService from './services/login'
 
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchBlogs, createBlog } from './reducers/blogReducer'
-// import { setNotification, setStyle } from './reducers/notificationReducer'
-
-import { emptyUser, logUser } from './reducers/userReducer'
+import { fetchBlogs } from './reducers/blogReducer'
 import { fetchAllUsers } from './reducers/usersReducer'
+import { emptyUser, logUser } from './reducers/userReducer'
 import { setLoggedUser } from './reducers/loggedUserReducer'
 
 const App = () => {
   const dispatch = useDispatch()
-
-  const [username, setUserName] = useState('')
-  const [password, setPassword] = useState('')
 
   const blogs = useSelector((state) => state.blogs)
   const users = useSelector((state) => state.users)
@@ -55,29 +45,6 @@ const App = () => {
     }
   }, [])
 
-  // const handleLogin = async (ev) => {
-  //   ev.preventDefault()
-
-  //   try {
-  //     const user = await loginService.login({
-  //       username,
-  //       password,
-  //     })
-
-  //     // window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
-  //     blogService.setToken(user.token)
-  //     dispatch(setLoggedUser(user))
-  //     dispatch(logUser(user))
-
-  //     setUserName('')
-  //     setPassword('')
-  //   } catch (exception) {
-  //     dispatch(setStyle('error'))
-  //     dispatch(setNotification('wrong credentials', 5))
-  //   }
-  // }
-
-  //! logout does not clear user or token
   const handleLogout = async (ev) => {
     ev.preventDefault()
     window.localStorage.removeItem('loggedBlogUser')
@@ -88,27 +55,6 @@ const App = () => {
     console.log('logout user: ', user.name)
     console.log('logout token: ', user.token)
   }
-
-  // const blogFormRef = useRef()
-
-  // TODO refactor login
-
-  // if (user === null) {
-  //   return (
-  //     <div>
-  //       <Notification />
-  //       <Togglable buttonLabel="login">
-  //         <LoginForm
-  //           username={username}
-  //           password={password}
-  //           handleUsernameChange={({ target }) => setUserName(target.value)}
-  //           handlePasswordChange={({ target }) => setPassword(target.value)}
-  //           handleSubmit={handleLogin}
-  //         />
-  //       </Togglable>
-  //     </div>
-  //   )
-  // }
 
   return (
     <div>
